59ff3407985f4db9eca2edec678f38e977972013
Vanessa <laylavanessa11@gmail.com>
2024-09-28 10:57:07 -0600

    principio

--- Exerc2/Exerc2/Program.cs
+++ Exerc2/Exerc2/Program.cs
@@ -1,13 +1,31 @@
 ﻿using System;
 
-namespace Exerc2
+namespace ListLINQ
 {
     class Program
     {
+        // Listas con LINQ
+        #region Classes
+        public class People {
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public eGender Gender { get; set; } // Femenino - Masculino - Indefinido
+        }
+        #endregion
+
+        #region MyRegion
+        public enum eGender
+        {
+            Undefined,// = 0,
+            Female, // = 1,
+            Male // = 2
+        }
+        #endregion
+
         static void Main(string[] args)
         {
             //
-
+            
             Console.ReadKey();
         }
     }
